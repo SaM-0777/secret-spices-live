65be7140af28873a339d8ec0947926d31c437fb9
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2022-10-31 00:14:44 +0530

    recipe getForDisplay & getDetails routes defined

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -14,6 +14,98 @@ export async function getAllRecipes(req, res) {
 };
 
 
+export async function getRecipesDisplay(req, res) {
+    const query = Recipe.aggregate([
+        {
+            "$lookup": {
+                "from": "authors",
+                "localField": "authorId",
+                "foreignField": "_id",
+                "as": "Author"
+            },
+        },
+        {
+            "$lookup": {
+                "from": "ratings",
+                "localField": "_id",
+                "foreignField": "recipeId",
+                "as": "Ratings",
+            },
+        },
+        {
+            "$lookup": {
+                "from": "views",
+                "localField": "_id",
+                "foreignField": "recipeId",
+                "as": "Views"
+            },
+        },
+        {
+            "$project": {"Author.name": 1, "Author.thumbnail": 1, "thumbnail": 1, "title": 1, "duration": 1, "vegOrNonVeg": 1, "createdAt": 1, "Ratings": {"$avg": "$Ratings.rating"}, "viewCount": {"$size": "$Views"} }
+        },
+    ])
+
+    query.exec(function (error, recipeDataForDisplay) {
+        if (error) res.status(500).json({ message: error.message })
+        res.status(200).json(recipeDataForDisplay)
+    })
+
+};
+
+
+export async function getRecipeDetailsByRecipeId(req, res) {
+    const { recipeId } = req.params
+
+    const query = Recipe.aggregate([
+        {
+            // match id
+            $match: { _id: mongoose.Types.ObjectId(recipeId) }
+        },
+        {
+            "$lookup": {
+                "from": "views",
+                "localField": "_id",
+                "foreignField": "recipeId",
+                "as": "Views",
+            },
+        },
+        {
+            "$lookup": {
+                "from": "ratings",
+                "localField": "_id",
+                "foreignField": "recipeId",
+                "as": "Ratings",
+            },
+        },
+        {
+            "$lookup": {
+                "from": "likes",
+                "localField": "_id",
+                "foreignField": "recipeId",
+                "as": "Likes",
+            },
+        },
+        {
+            "$lookup": {
+                "from": "authors",
+                "localField": "authorId",
+                "foreignField": "_id",
+                "as": "Author",
+            },
+        },
+        {
+            "$project": { "heroBanner": 1, "title": 1, "description": 1, "Author.thumbnail": 1, "Author.name": 1, "Author.authorSocials": 1, "Author.isVerified": 1, "viewCount": { "$size": "$Views" }, "Rating": { "Rating": { "$avg": "$Ratings.rating" }, "ratingCount": { "$size": "$Ratings" } }, "steps": 1, "ingridients": 1, "duration": 1, "budget": 1, "likeCount": {"$size": "$Likes"} }
+        },
+    ])
+
+    query.exec(function (error, recipeDetails) {
+        if (error) res.status(404).json({ message: error.message })
+        res.status(200).json(recipeDetails)
+    })
+
+};
+
+
 export async function createRecipe (req, res) {
     const { userId, authorId, cookbookId, thumbnail, heroBanner, title, description, steps, ingridients, nutrients, duration, budget, tags, categories, vegOrNonVeg, } = req.body
 
--- routes/userRoutes/recipe.js
+++ routes/userRoutes/recipe.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getAllRecipes, createRecipe } from "../../controllers/recipe.js";
+import { getAllRecipes, createRecipe, getRecipesDisplay, getRecipeDetailsByRecipeId } from "../../controllers/recipe.js";
 
 
 const RecipeRouter = express.Router();
@@ -15,7 +15,21 @@ RecipeRouter.get('/all', getAllRecipes);
 
 /**
  * Method   Desc
- * GET      Get all recipes
+ * GET      Get all recipes for display
+ */
+RecipeRouter.get('/display/all', getRecipesDisplay);
+
+
+/**
+ * Method   Desc
+ * GET      Get recipe details
+ */
+RecipeRouter.get('/details/:recipeId', getRecipeDetailsByRecipeId);
+
+
+/**
+ * Method   Desc
+ * POST     Create a recipe
  */
 RecipeRouter.post('/create/new', createRecipe);
 
