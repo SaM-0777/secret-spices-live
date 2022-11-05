dcd4c529b134cdc0157f40815ad8d786a28cbfc1
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2022-11-05 12:06:40 +0530

    dotenv for production changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 import mongoose from "mongoose";
 
 // Routes
